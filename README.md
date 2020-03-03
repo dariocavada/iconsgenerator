f86ce8745695c0382db3ce6688ef19b5f24223ed
dariocavada <dario.cavada.lab@gmail.com>
2020-03-03 11:12:10 +0100

    Icons Generator version 1.1

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Nodejs application to create splash screens and icons for android and ios apps
 - Download or clone the repository
 - From the command line `npm install`
 - Replace `splash.png` (must be 2732 × 2732 pixel) and `icon.png` ( must be 1024x1024 pixel) files inside the main folder
-- From the command line execute `npm start` the application reads the file `automations.json`, creates the ios and android folders with splash and icons subfolders and all the variations of icons and splash needed by Android and iOS.
+- From the command line execute `npm start` the application reads the file `automations.json`, it create the screens and icons folders with ios and android subfolders and all the variations of icons and splash needed by Android and iOS.
 
-
+# Dependencies
+- The application use `sharp` to resize and crop the various images
---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+
+*/
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
+var nextImg = 0;
+
+var jsonConfig = {};
+
+console.log("START generating ...")
+
+function resizeOne() {
+
+  var oc = jsonConfig.automations[nextImg];
+  var dirName = path.dirname(oc.out);
+  console.log("GENERATE image: ", oc.out);
+
+  fs.mkdirSync(dirName, { recursive: true });
+
+  sharp(oc.in)
+    .resize(oc.resize[0], oc.resize[1], {
+      kernel: sharp.kernel.nearest,
+      fit: 'contain',
+      background: jsonConfig.background
+    })
+    .toFile(oc.out)
+    .then(() => {
+      nextImg++;
+      if (nextImg < jsonConfig.automations.length) {
+        setTimeout(resizeOne, 10);
+      } else {
+        console.log("END automations")
+      }
+    });
+}
+
+let rawdata = fs.readFileSync('automations.json');
+jsonConfig = JSON.parse(rawdata);
+resizeOne();
+
